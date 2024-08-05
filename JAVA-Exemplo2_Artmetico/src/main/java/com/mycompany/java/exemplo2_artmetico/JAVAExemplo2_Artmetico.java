95bebeb8ebe89195f428d820b82ae864b7b5f5c9
José Vicente <160607761+JoseVicente-Carv@users.noreply.github.com>
2024-08-05 10:01:57 -0300

    Add files via upload

---
+++ JAVA-Exemplo2_Artmetico/src/main/java/com/mycompany/java/exemplo2_artmetico/JAVAExemplo2_Artmetico.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java.exemplo2_artmetico;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class JAVAExemplo2_Artmetico {
+
+    public static void main(String[] args) {
+        System.out.println("Segundo Exemplo - Atmético");
+        
+        //declaração de variaveis
+        int a = 10;
+        int b = 2;
+        
+        //operadores artméticos
+        int multiplicar = a * b;
+        int dividir = a / b;
+        int somar = a + b;
+        int subtrair = a - b;
+        
+        //exibindo resultados
+        System.out.println("Multiplicar: " + multiplicar);
+        System.out.println("Dividir: " + dividir);
+        System.out.println("Somar: " + somar);
+        System.out.println("Subtrair: " + subtrair);    
+    }
+}
